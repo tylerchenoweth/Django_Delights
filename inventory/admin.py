bb3daa27412657516c491e3fcb9105c6093e85ba
Tom Bombadil <tchen358@gmail.com>
2023-04-27 23:01:11 -0400

    Finished the models and added an example

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import MenuItem, Ingredient, RecipeRequirement, Purchases
+
 # Register your models here.
+admin.site.register(MenuItem)
+admin.site.register(Ingredient)
+admin.site.register(RecipeRequirement)
+admin.site.register(Purchases)
--- inventory/models.py
+++ inventory/models.py
@@ -7,32 +7,56 @@ class MenuItem(models.Model):
     title = models.CharField(max_length=50)
     price = models.FloatField(default=0.0)
 
+    def __str__(self):
+        return self.title
+
 class Ingredient(models.Model):
     
     CUP = "CUP"
     OUNCE = "OZS"
     TABLESPOON = "TBS"
     TEASPOON = "TSP"
-    
+    GRAM = "GRM"
+    BOX = "BOX"
+    SMALL = "SML"
+    MEDIUM = "MED"
+    LARGE = "LRG"
+    POUND = "LBS"
+
     UNIT_TYPES = [
         (CUP, "Cup"),
         (OUNCE, "Ounce"),
         (TABLESPOON, "Tablespoon"),
-        (TEASPOON, "Teaspoon")
+        (TEASPOON, "Teaspoon"),
+        (BOX, "Box"),
+        (GRAM,"Gram"),
+        (LARGE,"Large"),
+        (MEDIUM,"Medium"),
+        (SMALL,"Small"),
+        (POUND, "LBS"),
     ]
 
     name = models.CharField(max_length=30)
-    quantity = models.IntegerField(default=0)
-    unit = models.CharField(max_length=3, choices=UNIT_TYPES,)
+    quantity = models.FloatField(default=0)
+    unit = models.CharField(max_length=3, choices=UNIT_TYPES)
     unit_price = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.name
 
 class RecipeRequirement(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     quantity = models.FloatField(default=0.0)
 
+    def __str__(self):
+        return self.menu_item.title + " - " + self.ingredient.name
+
 class Purchases(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     amount_paid = models.FloatField(default=0.0)
     timestamp = models.DateTimeField(default=datetime.datetime.now)
+
+    def __str__(self):
+        return self.menu_item.title
     
