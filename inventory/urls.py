0e24dd0bbe6ec2d849e9796f2ba1c206239ba778
Tom Bombadil <tchen358@gmail.com>
2023-04-30 01:01:06 -0400

    Converted to class based views and html lists inventory

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("menu", views.menu, name="menu"),
-    path("inventory", views.inventory, name="inventory"),
+    path("inventory", views.InventoryList.as_view(), name="inventory"),
     path("purchases", views.purchases, name="purchases"),
     
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import Ingredient
+from django.views.generic import ListView
+
 # Create your views here.
 def home(request):
     return render(request, "inventory/home.html")
@@ -7,8 +10,10 @@ def home(request):
 def menu(request):
     return render(request, "inventory/menu.html")
 
-def inventory(request):
-    return render(request, "inventory/inventory.html")
+class InventoryList(ListView):
+    model = Ingredient
+    #return render(request, "inventory/inventory.html")
+    template_name = "inventory/inventory.html"
 
 def purchases(request):
     return render(request, "inventory/purchases.html")
