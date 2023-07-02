266a150d971e50a31811c60b50ddcdec409fc64a
Tom Bombadil <tchen358@gmail.com>
2023-07-02 13:41:05 -0400

    removed MenuItem field option from the RecipeRequirementCreateForm

--- inventory/forms.py
+++ inventory/forms.py
@@ -3,26 +3,22 @@ from .models import MenuItem, Ingredient, RecipeRequirement, Purchases
 
 class MenuItemCreateForm(forms.ModelForm):
     class Meta:
-        model = MenuItem
-        #fields = ("name", "quantity", "unit", "unit_price") 
+        model = MenuItem 
         fields = "__all__"
 
 class IngredientCreateForm(forms.ModelForm):
     class Meta:
         model = Ingredient
-        #fields = ("name", "quantity", "unit", "unit_price") 
         fields = "__all__"
 
 class PurchasesCreateForm(forms.ModelForm):
     class Meta:
         model = Purchases
-        #fields = ("name", "quantity", "unit", "unit_price") 
         fields = "__all__"
 
 class RecipeRequirementCreateForm(forms.ModelForm):
     class Meta:
         model = RecipeRequirement
-        #fields = ("name", "quantity", "unit", "unit_price") 
-        fields = "__all__"
+        fields = ("ingredient", "quantity") 
 
 
