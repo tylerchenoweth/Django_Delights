180d41eb4cbd6f704efd8f64438c557df1e74d84
sh4ck1et0n <tchen358@gmail.com>
2023-07-26 18:05:36 -0400

    initial data populating

--- inventory/factory.py
+++ inventory/factory.py
@@ -1,13 +1,40 @@
 import factory
 from factory.faker import faker
 
-from .models import MenuItem
+from .models import MenuItem, Ingredient, RecipeRequirement
 
 FAKE = faker.Faker()
 
-class MenuItemFactory(factory.django.DjangoModelFactory):
+
+from faker import Faker
+
+fake = Faker()
+
+ingredients_list = [
+    'cane sugar','flour', 'egg',
+    'milk chocolate', 'butter',
+    'orange jelly', 'banilla icing',
+    'dough', 'sprinkles'
+]
+
+class CustomIngredients(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Ingredient
+    
+    fake.sentence(ext_word_list=ingredients_list)
+
+"""class MenuItemFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = MenuItem
     
     title = factory.Faker("sentence", nb_words=2)
     price = factory.Faker("random_number")
+
+class RecipeRequirementFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = RecipeRequirement
+    
+    menu_item = #fk 
+    ingredient = #fk
+    quantity = factory.Faker("random_number")
+"""
