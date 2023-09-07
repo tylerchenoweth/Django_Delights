ee6acf3db47b8bcd244567db24996bcfd69f8098
sh4ck1et0n <tchen358@gmail.com>
2023-09-06 22:21:45 -0400

    This does not use docker or postgresql

--- djangodelights/settings.py
+++ djangodelights/settings.py
@@ -82,12 +82,9 @@ WSGI_APPLICATION = 'djangodelights.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "db",  # set in docker-compose.yml
-        "PORT": 5432,  # default postgres port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
     }
 }
 
