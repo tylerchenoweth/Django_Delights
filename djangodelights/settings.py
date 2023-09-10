11b367b7ae8103ff0bdde212d3f4ed0ba85cab6f
sh4ck1et0n <tchen358@gmail.com>
2023-09-10 12:02:42 -0400

    changed production settings back to dev. The production version is in the branch 'Production_Main'

--- djangodelights/settings.py
+++ djangodelights/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=lo@7%its*sd&t46(_71)9i@q!@_2s5@er#n1a#9_8szkjt366'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
@@ -136,7 +136,8 @@ LOGOUT_REDIRECT_URL = "/accounts/login/"
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# Used for production
+#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Must add this for CSS
 STATICFILES_DIRS = ( 
